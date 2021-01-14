c819c93433e6b614824f5a8c9beef4e9e873884f
ebernevec <eduard.bernevec8@gmail.com>
2021-01-14 11:55:27 +0200

    update with new methods files Test2 and FourthTask

--- src/com/company/FourthTask.java
+++ src/com/company/FourthTask.java
@@ -1,5 +1,12 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class FourthTask {
     /*comment from FourthTask*/
+    public void Mouse(){
+        System.out.println("Hello Mouse");
+    }
 }
--- src/com/company/Test2.java
+++ src/com/company/Test2.java
@@ -2,4 +2,7 @@ package com.company;
 
 public class Test2 {
     /* comment from Test2 */
+    void Cat(){
+        System.out.println("Hello Cat");
+    }
 }
