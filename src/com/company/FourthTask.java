13152d8d0ce11a787987f859080ad242d7496c67
ebernevec <eduard.bernevec8@gmail.com>
2021-01-14 10:34:55 +0200

    Create new branch task4 and a new class FourthTask

---
+++ src/com/company/FourthTask.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class FourthTask {
+    /*comment from FourthTask*/
+}
--- src/com/company/Test2.java
+++ src/com/company/Test2.java
@@ -1,7 +1,5 @@
 package com.company;
 
 public class Test2 {
-    void myClass{
-        System.out.println("Hellos Second Branch");
-    }
+    /* comment from Test2 */
 }
