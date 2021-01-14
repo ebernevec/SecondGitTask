732e712ba61b18a07e20532da54deabccc370836
ebernevec <eduard.bernevec8@gmail.com>
2021-01-14 12:56:58 +0200

    delete imports from file FourthTask

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello WWWWorld te te test fifthTask!");
+        System.out.println("Hello WWWWorld re te test fifthTask!");
     }
 }
